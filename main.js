48b7ee37f072e246ab58611cc9ce60b2d74787bd
hassan_ashraf_ <hassan@coinswitch.co>
2021-04-22 22:49:51 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+const FormData = require('form-data');
+const axios = require('axios');
+const fs = require('fs');
+
+const form = new FormData();
+
+form.append('filename', '')
+form.append('attachment', fs.createReadStream('/Users/syedhassanashraf/Desktop/latest.png'));
+
+const headers = form.getHeaders();
+
+headers.authorization = `pk_3795789_GEEHSYVKLQYOK341LRIZ0JYFAWK8PXBX`;
+
+axios({
+  method: 'post',
+  url: 'https://api.clickup.com/api/v2/task/7zw3vy/attachment',
+  data: form,
+  headers,
+})
+  .then(() => console.log('success'))
+  .catch((e) => console.log(e));
